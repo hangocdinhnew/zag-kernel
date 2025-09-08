a0bf8b17d0dcc9c29ddc062193a8c484d1f29576
hangocdinhnew <dinhndrwnew@gmail.com>
2025-09-08 10:11:08 +0700

    Add a carriage return. (Why UEFI)

--- src/bootloader/main.zig
+++ src/bootloader/main.zig
@@ -9,5 +9,5 @@ pub fn ziggy_bootloader_main() void {
     const con_out = uefi.system_table.con_out.?;
     _ = con_out.reset(true) catch unreachable;
     _ = con_out.clearScreen() catch unreachable;
-    _ = con_out.outputString(std.unicode.utf8ToUtf16LeStringLiteral("Hello, World\n")) catch unreachable;
+    _ = con_out.outputString(std.unicode.utf8ToUtf16LeStringLiteral("Hello, World\r\n")) catch unreachable;
 }
